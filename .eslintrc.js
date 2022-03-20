23355736a193751bd36c0b4bfda35b09d6a3ab66
Galileo01 <1354541676@qq.com>
2022-03-20 19:22:41 +0800

    feat: eslint 规则打包 @galileo01/eslint-config-react-ts 并extends 使用

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,45 +3,5 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    // react
-    'plugin:react/recommended',
-    // airbnb + airbnb 推荐的 ts 规范
-    'airbnb',
-    'airbnb-typescript',
-    // 解决 eslint 和 prettier 的冲突 , 此项配置必须在最后
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    // 开启 hooks 规则
-    'react-hooks/rules-of-hooks': 'error',
-    // 打开 检查 effect 依赖 ，默认关闭
-    'react-hooks/exhaustive-deps': 'warn',
-    // 覆盖 eslint-config-airbnb 和 react 里的配置
-    //  允许 在ts、tsx 中书写 jsx
-    'react/jsx-filename-extension': [
-      2,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
-    // 修改 对于 函数式组件 声明方式(箭头函数 or 函数声明)的 校验
-    'react/function-component-definition': [
-      'error',
-      {
-        namedComponents: ['arrow-function', 'function-declaration'],
-        unnamedComponents: ['arrow-function'],
-      },
-    ],
-    // 关闭 对文件扩展名的 校验
-    'import/extensions': 'off',
-  },
+  extends: ['@galileo01/eslint-config-react-ts'],
 }
